1431fcf83a27ad1b7dc6b6ee29bba3ae48f126e5
yeomseungbeen <storm6781@naver.com>
2026-08-07 17:17:18 +0900

    fix: ChatRoomRequestDto Setter 누락으로 인한 데이터 바인딩 오류 수정

--- src/main/java/org/makery/websocket/ChatRoomRequestDto.java
+++ src/main/java/org/makery/websocket/ChatRoomRequestDto.java
@@ -2,8 +2,10 @@ package org.makery.websocket;
 
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @Getter
+@Setter
 @NoArgsConstructor
 public class ChatRoomRequestDto {
 
