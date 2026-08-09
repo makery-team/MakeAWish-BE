a0ace46915b66833c76b11d8dc41c2c4f6d62993
yeomseungbeen <storm6781@naver.com>
2026-08-09 14:09:16 +0900

    fix(store): hours 컬럼 타입 TEXT로 변경 (JSON 문자열 길이 이슈 해결)

--- src/main/java/org/makery/domain/Store.java
+++ src/main/java/org/makery/domain/Store.java
@@ -33,7 +33,9 @@ public class Store extends BaseEntity {
     @Column(length = 50)
     private String phone;
 
+    @Column(columnDefinition = "TEXT")
     private String hours;
+    
     private String notice; // 사장님 한마디/공지
 
     /**
