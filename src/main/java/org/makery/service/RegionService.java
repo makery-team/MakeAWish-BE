fed62f634f93b5d6fb79f3123e1f9ca2abf6f4a2
yeomseungbeen <storm6781@naver.com>
2026-08-10 14:14:55 +0900

    fix: PostConstruct 임포트 패키지를 jakarta로 변경 (빌드 에러 해결)

--- src/main/java/org/makery/service/RegionService.java
+++ src/main/java/org/makery/service/RegionService.java
@@ -8,7 +8,7 @@ import org.makery.dto.RegionResponse;
 import org.springframework.core.io.ClassPathResource;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.PostConstruct;
+import jakarta.annotation.PostConstruct;
 import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
